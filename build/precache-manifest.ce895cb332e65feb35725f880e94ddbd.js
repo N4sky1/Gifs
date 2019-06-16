a3f072ad1cc41218f568a5990525b7498295efe8
N4sky1 <N4sky@yandex.ru>
2019-06-16 13:59:35 +0700

    changes

---
+++ build/precache-manifest.ce895cb332e65feb35725f880e94ddbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72778b835c76de08d59781d58381e5d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e62d694c3346a1cba8f1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e62d694c3346a1cba8f1",
+    "url": "/static/js/main.c6a7eb4e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
